502a8eb7d0c7e8ea06ddbf2548cd7fd2cdb30f8e
EtoileSKS <146693422+EtoileSKS@users.noreply.github.com>
2024-03-05 14:02:25 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -19,7 +19,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '普罗诺亚天文台', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || 'Étoile_Sakasa宇内观测年鉴', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://etoilesakasa.xyz', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || '', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
